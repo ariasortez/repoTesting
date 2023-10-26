cfd045beb13ecff7504a10c65bbc60b6ad938f8a
ariasortez <juan.arias@unitec.edu>
2023-10-25 19:48:03 -0600

    Mongo Connection

--- Semana 1/helloworld.js
+++ Semana 1/helloworld.js
@@ -1,19 +1,51 @@
-const express = require("express");
+import express from "express";
+import mongoose from "mongoose";
+import Users from "./models/users.model.js";
+import "dotenv/config";
+
 const app = express();
-const port = 3000;
+
+app.use(express.json());
+
+const port = process.env.PORT;
+// Connection URL
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGOOSE_KEY, {
+      useNewUrlParser: true,
+    });
+    console.log("DB is connected");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/home", (req, res) => {
-  res.send("Scrum Master");
+app.get("/test", (req, res) => {
+  res.send("Test Page");
 });
 
-app.get("/contact", (req, res) => {
-  res.send("Contact");
+app.post("/register", async (req, res) => {
+  const { name, age } = req.body;
+  const newUsers = new Users({
+    name,
+    age,
+  });
+
+  await newUsers.save();
+
+  res.status(200).send({
+    ok: true,
+    message: "User has been added successfully",
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
